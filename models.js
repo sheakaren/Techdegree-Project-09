9008791c54826b5f4b2bb9e857b08a070c298ff5
Karen Shea <shea.karene@gmail.com>
2019-04-02 17:48:46 -0400

    Added field requirements in schemas

--- models.js
+++ models.js
@@ -5,20 +5,28 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    emailAddress: String,
-    password: String
+    firstName: {type: String,
+                required: [true, 'First name is required']},
+    lastName: {type: String,
+                required: [true, 'Lase name is required']},
+    emailAddress: {type: String,
+                required: [true, 'Email address is required']},
+    password: {type: String,
+                required: [true, 'Password is required']}
 });
 
 var User = mongoose.model('User', UserSchema);
 
 var CourseSchema = new Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref:"User"},
-    title: String,
-    description: String,
-    estimatedTime: String,
-    materialsNeeded: String
+    title: {type: String,
+                required: [true, 'Course title is required']},
+    description: {type: String,
+                required: [true, 'Course description is required']},
+    estimatedTime: {type: String,
+                required: [true, 'Estimated time is required']},
+    materialsNeeded: {type: String,
+                required: [true, 'materials needed is required']}
 });
 
 var Course = mongoose.model('Course', CourseSchema);
